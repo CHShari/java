16e417293048c019ac458acb1fac5f158d4a53c1
CHShari <chinthaguntasrihari03@gmail.com>
2022-10-27 22:58:03 +0530

    Increment

---
+++ Increment.java
@@ -0,0 +1,12 @@
+class Increment {
+	public static void main(String args[]) {
+		int a=20;
+		System.out.println("the value of a is:"+a);
+		System.out.println("the value of a is after post increment of a is"+(--a));
+		System.out.println();
+		int b=30;
+		System.out.println("the value of b is"+b);
+		System.out.println("the value of b is after pre increment of b is "+(b--));
+		System.out.println("the value of b is"+b);
+	}
+}
